6e731dbf3e173d4a01a2c1292b17da36d565ad93
mongte <mongte32@gmail.com>
2021-04-28 23:51:17 +0900

    feat: mongte

--- mongte/basic/03/script.js
+++ mongte/basic/03/script.js
@@ -1,23 +1,22 @@
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600
-}
-
 // Scene
 const scene = new THREE.Scene()
 
 // Object
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
 const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: '#ff0000'
+  color: '#ff0000'
 })
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
 scene.add(cubeMesh)
 
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600
+}
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
@@ -25,7 +24,8 @@ scene.add(camera)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
